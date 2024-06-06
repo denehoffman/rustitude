4165028a438fb8d685d25abb92a43ff0c99c15ee
denehoffman <denehoffman@me.com>
2024-06-06 12:29:53 -0400

    style: ensure clippy lints for perf and style

--- crates/rustitude-core/src/lib.rs
+++ crates/rustitude-core/src/lib.rs
@@ -258,6 +258,8 @@
     clippy::missing_safety_doc,
     clippy::missing_panics_doc,
     clippy::missing_errors_doc,
+    clippy::perf,
+    clippy::style,
     missing_docs
 )]
 #![cfg_attr(feature = "simd", feature(portable_simd))]
