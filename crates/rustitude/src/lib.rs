b3362440b5bcb641f3908927a0a21ad00b4e3529
denehoffman <denehoffman@me.com>
2024-07-20 11:31:19 -0400

    docs: add crate-level docs for rustitude to point to rustitude-core and rustitude-gluex

--- crates/rustitude/src/lib.rs
+++ crates/rustitude/src/lib.rs
@@ -1,3 +1,11 @@
+//! # Rustitude
+//! ## Demystifying Amplitude Analysis with Rust and Python
+//!
+//! The `rustitude` crate is an interface for both using amplitudes written in Rust and for writing
+//! new amplitudes. There is quite a bit of additional documentation in the
+//! [`rustitude-core`](`rustitude_core`) crate, and this crate just re-exports the prelude as well
+//! as the amplitudes located in the [`rustitude-gluex`](`rustitude_gluex`) crate. See the
+//! respective crates for more in-depth information.
 pub mod prelude {
     pub use rustitude_core::prelude::*;
 }
