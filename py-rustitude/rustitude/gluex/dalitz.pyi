f44585b9fcae2bff4273b0edbcf9cc3a62a80d61
denehoffman <denehoffman@me.com>
2024-07-31 13:51:25 -0400

    style: remove unneeded noqa's

--- py-rustitude/rustitude/gluex/dalitz.pyi
+++ py-rustitude/rustitude/gluex/dalitz.pyi
@@ -1,6 +1,6 @@
 from rustitude import Amplitude, Amplitude_64, Amplitude_32
 from rustitude.gluex import Decay
 
-def OmegaDalitz(name: str, decay: Decay = Decay.ThreeBodyDecay([0, 1, 2])) -> Amplitude: ...  # noqa: N802
-def OmegaDalitz_64(name: str, decay: Decay = Decay.ThreeBodyDecay([0, 1, 2])) -> Amplitude_64: ...  # noqa: N802
-def OmegaDalitz_32(name: str, decay: Decay = Decay.ThreeBodyDecay([0, 1, 2])) -> Amplitude_32: ...  # noqa: N802
+def OmegaDalitz(name: str, decay: Decay = Decay.ThreeBodyDecay([0, 1, 2])) -> Amplitude: ...
+def OmegaDalitz_64(name: str, decay: Decay = Decay.ThreeBodyDecay([0, 1, 2])) -> Amplitude_64: ...
+def OmegaDalitz_32(name: str, decay: Decay = Decay.ThreeBodyDecay([0, 1, 2])) -> Amplitude_32: ...
