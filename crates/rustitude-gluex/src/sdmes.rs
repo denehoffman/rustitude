5447787f7a98b64254723e469f76fe5c12aeb4a1
denehoffman <denehoffman@me.com>
2024-07-31 18:23:24 -0400

    fix: made some minor changes to SDMEs which were written incorrectly

--- crates/rustitude-gluex/src/sdmes.rs
+++ crates/rustitude-gluex/src/sdmes.rs
@@ -41,8 +41,8 @@ impl<F: Field> Node<F> for TwoPiSDME<F> {
                 );
                 let pgamma = event.eps.norm();
                 (
-                    p.theta_cos(),
-                    p.theta().fpowi(2),
+                    p.theta_cos().fpowi(2),
+                    F::fsin(p.theta()).fpowi(2),
                     F::fsin(F::TWO * p.theta()),
                     p.phi(),
                     big_phi,
@@ -54,9 +54,7 @@ impl<F: Field> Node<F> for TwoPiSDME<F> {
     }
 
     fn calculate(&self, parameters: &[F], event: &Event<F>) -> Result<Complex<F>, RustitudeError> {
-        let (costheta, sinsqtheta, sin2theta, phi, big_phi, pgamma) = self.data[event.index];
-        let pol_angle = event.eps[0].facos();
-        let r_big_phi = pol_angle * F::f(0.017453293) + big_phi;
+        let (cossqtheta, sinsqtheta, sin2theta, phi, big_phi, pgamma) = self.data[event.index];
         let rho_000 = parameters[0];
         let rho_100 = parameters[1];
         let rho_1n10 = parameters[2];
@@ -70,20 +68,20 @@ impl<F: Field> Node<F> for TwoPiSDME<F> {
         Ok(F::fsqrt(F::fabs(
             (F::THREE / (F::FOUR * F::PI()))
                 * (F::f(0.5) * (F::ONE - rho_000)
-                    + F::f(0.5) * (F::THREE * rho_000 - F::ONE) * costheta * costheta
+                    + F::f(0.5) * (F::THREE * rho_000 - F::ONE) * cossqtheta
                     - F::SQRT_2() * rho_100 * sin2theta * F::fcos(phi)
-                    - rho_1n10 * F::fcos(F::TWO * phi))
+                    - rho_1n10 * sinsqtheta * F::fcos(F::TWO * phi))
                 - pgamma
-                    * F::fcos(F::TWO * r_big_phi)
-                    * (rho_111 * sinsqtheta + rho_001 * costheta * costheta
+                    * F::fcos(F::TWO * big_phi)
+                    * (rho_111 * sinsqtheta + rho_001 * cossqtheta
                         - F::SQRT_2() * rho_101 * sin2theta * F::fcos(phi)
                         - rho_1n11 * sinsqtheta * F::fcos(F::TWO * phi))
                 - pgamma
-                    * F::fsin(F::TWO * r_big_phi)
+                    * F::fsin(F::TWO * big_phi)
                     * (F::SQRT_2() * rho_102 * sin2theta * F::fsin(phi)
                         + rho_1n12 * sinsqtheta * F::fsin(F::TWO * phi)),
         ))
-        .into())
+        .c())
     }
 
     fn parameters(&self) -> Vec<String> {
@@ -128,9 +126,18 @@ impl<F: Field> Node<F> for ThreePiSDME<F> {
             .events
             .par_iter()
             .map(|event| {
-                let (_, y, _, p) = self.decay.coordinates(self.frame, 0, event);
-
-                let big_phi = y.dot(&event.eps).fatan2(
+                let res_p4 = self.decay.resonance_p4(event);
+                let p1_res_p4 = self.decay.primary_p4(event).boost_along(&res_p4);
+                let p2_res_p4 = self.decay.primary_p4(event).boost_along(&res_p4);
+                let norm = p1_res_p4
+                    .momentum()
+                    .cross(&p2_res_p4.momentum())
+                    .normalize();
+                let (_, y, _, p) = self
+                    .frame
+                    .coordinates_from_boosted_vec(self.decay, &norm, event);
+                let big_phi = F::fatan2(
+                    y.dot(&event.eps),
                     event
                         .beam_p4
                         .momentum()
@@ -139,8 +146,8 @@ impl<F: Field> Node<F> for ThreePiSDME<F> {
                 );
                 let pgamma = event.eps.norm();
                 (
-                    p.theta_cos(),
-                    p.theta().fpowi(2),
+                    p.theta_cos().fpowi(2),
+                    F::fsin(p.theta()).fpowi(2),
                     F::fsin(F::TWO * p.theta()),
                     p.phi(),
                     big_phi,
@@ -152,9 +159,7 @@ impl<F: Field> Node<F> for ThreePiSDME<F> {
     }
 
     fn calculate(&self, parameters: &[F], event: &Event<F>) -> Result<Complex<F>, RustitudeError> {
-        let (costheta, sinsqtheta, sin2theta, phi, big_phi, pgamma) = self.data[event.index];
-        let pol_angle = event.eps[0].facos();
-        let r_big_phi = pol_angle * F::f(0.017453293) + big_phi;
+        let (cossqtheta, sinsqtheta, sin2theta, phi, big_phi, pgamma) = self.data[event.index];
         let rho_000 = parameters[0];
         let rho_100 = parameters[1];
         let rho_1n10 = parameters[2];
@@ -168,20 +173,20 @@ impl<F: Field> Node<F> for ThreePiSDME<F> {
         Ok(F::fsqrt(F::fabs(
             (F::THREE / (F::FOUR * F::PI()))
                 * (F::f(0.5) * (F::ONE - rho_000)
-                    + F::f(0.5) * (F::THREE * rho_000 - F::ONE) * costheta * costheta
+                    + F::f(0.5) * (F::THREE * rho_000 - F::ONE) * cossqtheta
                     - F::SQRT_2() * rho_100 * sin2theta * F::fcos(phi)
-                    - rho_1n10 * F::fcos(F::TWO * phi))
+                    - rho_1n10 * sinsqtheta * F::fcos(F::TWO * phi))
                 - pgamma
-                    * F::fcos(F::TWO * r_big_phi)
-                    * (rho_111 * sinsqtheta + rho_001 * costheta * costheta
+                    * F::fcos(F::TWO * big_phi)
+                    * (rho_111 * sinsqtheta + rho_001 * cossqtheta
                         - F::SQRT_2() * rho_101 * sin2theta * F::fcos(phi)
                         - rho_1n11 * sinsqtheta * F::fcos(F::TWO * phi))
                 - pgamma
-                    * F::fsin(F::TWO * r_big_phi)
+                    * F::fsin(F::TWO * big_phi)
                     * (F::SQRT_2() * rho_102 * sin2theta * F::fsin(phi)
                         + rho_1n12 * sinsqtheta * F::fsin(F::TWO * phi)),
         ))
-        .into())
+        .c())
     }
 
     fn parameters(&self) -> Vec<String> {
