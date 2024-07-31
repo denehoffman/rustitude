2e59d2f8636d8ab798025e3a86b3d3bdc12c6eee
denehoffman <denehoffman@me.com>
2024-07-31 13:52:10 -0400

    feat: add `ThreePiPolFrac` amplitude (mirroring `ThreePiAngles.cc` in `halld_sim`)

--- crates/rustitude-gluex/src/lib.rs
+++ crates/rustitude-gluex/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod dalitz;
 pub mod harmonics;
+pub mod polarization;
 pub mod resonances;
 pub mod sdmes;
 pub mod utils;
--- py-rustitude/src/gluex/mod.rs
+++ py-rustitude/src/gluex/mod.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 mod dalitz;
 mod harmonics;
+mod polarization;
 mod resonances;
 mod sdmes;
 use crate::add_submodule;
@@ -10,6 +11,7 @@ pub fn pyo3_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
     add_submodule(m, "rustitude.gluex.resonances", resonances::pyo3_module)?;
     add_submodule(m, "rustitude.gluex.harmonics", harmonics::pyo3_module)?;
     add_submodule(m, "rustitude.gluex.dalitz", dalitz::pyo3_module)?;
+    add_submodule(m, "rustitude.gluex.polarization", polarization::pyo3_module)?;
     m.add_class::<rustitude_gluex::utils::Wave>()?;
     m.add_class::<rustitude_gluex::utils::Frame>()?;
     m.add_class::<rustitude_gluex::utils::Sign>()?;
